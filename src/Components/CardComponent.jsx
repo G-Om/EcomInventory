2742b9a309410cb68cb1eef83fffe77a6c6f18a8
om.gavade <115257951+G-Om@users.noreply.github.com>
2023-08-03 18:00:54 +0530

    Card Component added

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -1,9 +1,21 @@
+import tshirt from '../images/Tshirt.jpg'
 
-export const CardComponent = ()=> {
 
+export const CardComponent = ({image, category, name, price}) => {
     return (
-        <div>
-            CardComponent
+        <div class="card-div">
+            <div className='card-image-div'>
+                <img class="card-img" src={image} alt="Card image cap"></img>
+            </div>  
+            <div class="card-textbox"> 
+                <span class="product-category">{category} </span>
+                <a href="https://websitedemos.net/t-shirts-store-04/product/t-shirt-name-8/" class="product-link">
+                    <h2 class="product-title">{name}</h2>
+                </a>
+                <span class="price">
+                    <span class="woocommerce-Price-amount amount">
+                        <bdi><span class="woocommerce-Price-currencySymbol">$  </span>{price}</bdi></span></span>
+            </div>
         </div>
     )
-}
+};
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,12 @@
+import { CardComponent } from "../Components/CardComponent"
+import tshirt from '../images/Tshirt.jpg'
+
 
 export const HomePage = ()=>{
     return(
         <div>
             <b>Home component</b>
+            <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent>
         </div>
     )
 }
