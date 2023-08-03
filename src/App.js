dd877934ab79a8389200b7e4cdf9c62ac6c067d5
Sreyash <sreyashkunkekar2889@gmail.com>
2023-08-03 17:22:34 +0530

    initial footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { MainComponent } from './MainComponent';
-
+import { FooterComponent } from './Components/FooterComponent';
 function App() {
   return (
     <div className="App">
       <MainComponent></MainComponent>
+      <FooterComponent></FooterComponent>
     </div>
   );
 }
--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -1,10 +1,46 @@
-
-
-export const FooterComponent = ()=> {
-
-    return (
-        <div>
-            FooterComponent
+export const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footercontent">
+          <div className="subscribe">
+            <h3 className="heading">Subscribe to get offer in your inbox </h3>
+            <p className="discription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consectetur aperiam nisi error 
+            </p>
+            <div className="subscribeInput">
+              <form>
+                <input type="text" className="inputText" placeholder="Your Email Address..."></input>
+                <input type="submit" className="submit-btn" value="Subscribe"></input>
+              </form>
+            </div>
+            <div className="nav-elements-list">
+              <ul className="navElements">
+                <a href="#"><li>Buy T-Shirts</li></a>
+                <a href="#"><li>Men</li></a>
+                <a href="#"><li>Women</li></a>
+                <a href="#"><li>About</li></a>
+                <a href="#"> <li>contact</li></a>
+              </ul>
+            </div>
+            <div className="socail-media-handles">
+              <a href="#" target="_blank">
+                <img src="fb-icon.png"></img>
+              </a>
+              <a href="#" target="_blank">
+                <img src="instagram-icon.png"></img>
+              </a>
+              <a href="#" target="_blank">
+                <img src="twitter-icon.png"></img>
+              </a>
+            </div>
+            <div className="copyright-content">
+              <p>Copyright @ 2023</p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
