359f5527e5b181ffc22d949c4b4bc10991839099
Sreyash <sreyashkunkekar2889@gmail.com>
2023-08-05 00:22:17 +0530

    carousel added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,36 @@
-import { CardComponent } from "../Components/CardComponent"
-import tshirt from '../images/Tshirt.jpg'
-
-
-export const HomePage = ()=>{
-    return(
-        <div>
-            <b>Home component</b>
-            <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent>
+import { CardComponent } from "../Components/CardComponent";
+import tshirt from "../images/Tshirt.jpg";
+import carousel1 from "../images/carousel1.png";
+import carousel2 from "../images/carsousel1-bg.png";
+export const HomePage = () => {
+  return (
+    <div>
+      <b>Home component</b>
+      <section class="carousel" id="home">
+        <div className="carousel-text">
+          <div class="carousel-gender-text">
+            <h6>Women</h6>
+          </div>
+          <div className="carousel-tag">
+            <h1>Slick. Modern. Awesome.</h1>
+          </div>
+          <div className="carousel-button">
+            <a href="">Shop Collection</a>
+          </div>
         </div>
-    )
-}
+        
+        <div class="carousel-image">
+          <img class="front" src={carousel2} alt="" />  
+          <img class="back" src={carousel1} alt="" />  
+        </div>
+      </section>
+
+      <CardComponent
+        price={"5"}
+        name={"T-Shirt 1"}
+        category={"Men"}
+        image={tshirt}
+      ></CardComponent>
+    </div>
+  );
+};
