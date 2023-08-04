b985ad080f58f756e09680dbe4bd6f5335b2fb73
Mayur Panchal <mayurpanchal527@gmail.com>
2023-08-04 23:39:22 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { MainComponent } from './MainComponent';
 import { FooterComponent } from './Components/FooterComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,7 +5,6 @@ import tshirt from '../images/Tshirt.jpg'
 export const HomePage = ()=>{
     return(
         <div>
-            <b>Home component</b>
             <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent>
         </div>
     )
