444b583fdbb81a8b8bb31af9d95ee01dd2fac374
om.gavade <115257951+G-Om@users.noreply.github.com>
2023-08-05 21:55:13 +0530

    Woring on big tray homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,8 @@
 import { CardComponent } from "../Components/CardComponent";
- 
+
 export const HomePage = () => {
   return (
     <div>
-      <b>Home component</b>
       <section class="carousel" id="home">
         <div className="carousel-text">
           <div class="carousel-gender-text">
@@ -16,47 +15,105 @@ export const HomePage = () => {
             <a href="">Shop Collection</a>
           </div>
         </div>
-        
+
         <div class="carousel-image">
-          <img class="front" src={"./images/carsousel1-bg.png"} alt="" />  
-          <img class="back" src={"./images/carousel1.png"} alt="" />  
+          <img class="front" src={"./images/carsousel1-bg.png"} alt="" />
+          <img class="back" src={"./images/carousel1.png"} alt="" />
         </div>
       </section>
       <section className="card-tray">
-                <div className="card-container">
-                        <div className="card-inner">
+        <div className="card-container">
+          <div className="card-inner">
+            <div class="tray-heading-cont">
+              <div class="widget-container">
+                <h6 class="text">Summer Collection</h6>
+              </div>
+            </div>
 
-                            <div class="tray-heading-cont" >
-                                <div class="widget-container">
-                                    <h6 class="text">Summer Collection</h6>
-                                </div>
-                            </div>
+            <div class="tray-heading-cont">
+              <div class="widget-container-bg">
+                <h2>Popular T-Shirts</h2>{" "}
+              </div>
+            </div>
+            {/* Card Tray */}
+            <ul className="products column-4">
+              <li className="product-card">
+                <CardComponent
+                  price={"5"}
+                  name={"T-Shirt Name 1"}
+                  category={"Men"}
+                  image={"./images/card1.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"10"}
+                  name={"T-Shirt Name 2"}
+                  category={"Women"}
+                  image={"./images/card2.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"12"}
+                  name={"T-Shirt Name 3"}
+                  category={"Men"}
+                  image={"./images/card3.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"15"}
+                  name={"T-Shirt Name 4"}
+                  category={"Women"}
+                  image={"./images/card4.jpg"}
+                ></CardComponent>
+              </li>
+            </ul>
 
-                            <div class="tray-heading-cont">
-                                <div class="widget-container-bg">
-                                <h2 >Popular T-Shirts</h2> </div>
-                            </div>
-                            {/* Card Tray */}
-                            <ul className="products column-4">
-                                <li className="product-card">
-                                    <CardComponent price={"5"} name={"T-Shirt Name 1"} category={"Men"} image={"./images/card1.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"10"} name={"T-Shirt Name 2"} category={"Women"} image={"./images/card2.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"12"} name={"T-Shirt Name 3"} category={"Men"} image={"./images/card3.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"15"} name={"T-Shirt Name 4"} category={"Women"} image={"./images/card4.jpg"}></CardComponent>
-                                </li>
-                            </ul>    
+            {/* <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent> */}
+          </div>
+        </div>
+      </section>
 
-                            {/* <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent> */}
-                        </div>
-                </div>
-            </section>
-       
+      {/* 2 Big images tray */}
+      <section className="tray-big">
+        <div className="tray-cont col">
+          <div className="bigtray-img row-6">
+            <div className="img1-cont">
+              <div className="img-padding">
+                <a href="#">
+                  <img src="./images/collection-02.jpg"></img>
+                </a>
+              </div>
+            </div>
+            <div className="category">
+              <div className="text-populated">
+                <h6>MEN</h6>
+              </div>
+            </div>
+            <div className="widget-heading-outer">
+              <div className="heading-container">
+                <h2 className="heading">
+                  The base collection - Ideal every day.
+                </h2>
+              </div>
+            </div>
+            <div style={{ width: "100%" }}>
+              <div className="text-populated">
+                <a href="#">
+                  <span class="button-wrapper">
+                    <span class="button-text">Shop Now</span>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="bigtray-img row-6">
+            <div className="big-img"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
