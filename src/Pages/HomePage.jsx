a93d49b68e9cee888235c301226e3169ca0796a0
Sreyash <sreyashkunkekar2889@gmail.com>
2023-08-06 15:40:50 +0530

    Added elementor

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { CardComponent } from "../Components/CardComponent";
- 
+
 export const HomePage = () => {
   return (
     <div>
@@ -16,47 +16,93 @@ export const HomePage = () => {
             <a href="">Shop Collection</a>
           </div>
         </div>
-        
+
         <div class="carousel-image">
-          <img class="front" src={"./images/carsousel1-bg.png"} alt="" />  
-          <img class="back" src={"./images/carousel1.png"} alt="" />  
+          <img class="front" src={"./images/carsousel1-bg.png"} alt="" />
+          <img class="back" src={"./images/carousel1.png"} alt="" />
         </div>
       </section>
       <section className="card-tray">
-                <div className="card-container">
-                        <div className="card-inner">
+        <div className="card-container">
+          <div className="card-inner">
+            <div class="tray-heading-cont">
+              <div class="widget-container">
+                <h6 class="text">Summer Collection</h6>
+              </div>
+            </div>
 
-                            <div class="tray-heading-cont" >
-                                <div class="widget-container">
-                                    <h6 class="text">Summer Collection</h6>
-                                </div>
-                            </div>
+            <div class="tray-heading-cont">
+              <div class="widget-container-bg">
+                <h2>Popular T-Shirts</h2>{" "}
+              </div>
+            </div>
+            {/* Card Tray */}
+            <ul className="products column-4">
+              <li className="product-card">
+                <CardComponent
+                  price={"5"}
+                  name={"T-Shirt Name 1"}
+                  category={"Men"}
+                  image={"./images/card1.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"10"}
+                  name={"T-Shirt Name 2"}
+                  category={"Women"}
+                  image={"./images/card2.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"12"}
+                  name={"T-Shirt Name 3"}
+                  category={"Men"}
+                  image={"./images/card3.jpg"}
+                ></CardComponent>
+              </li>
+              <li className="product-card">
+                <CardComponent
+                  price={"15"}
+                  name={"T-Shirt Name 4"}
+                  category={"Women"}
+                  image={"./images/card4.jpg"}
+                ></CardComponent>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
-                            <div class="tray-heading-cont">
-                                <div class="widget-container-bg">
-                                <h2 >Popular T-Shirts</h2> </div>
-                            </div>
-                            {/* Card Tray */}
-                            <ul className="products column-4">
-                                <li className="product-card">
-                                    <CardComponent price={"5"} name={"T-Shirt Name 1"} category={"Men"} image={"./images/card1.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"10"} name={"T-Shirt Name 2"} category={"Women"} image={"./images/card2.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"12"} name={"T-Shirt Name 3"} category={"Men"} image={"./images/card3.jpg"}></CardComponent>
-                                </li>
-                                <li className="product-card">
-                                    <CardComponent price={"15"} name={"T-Shirt Name 4"} category={"Women"} image={"./images/card4.jpg"}></CardComponent>
-                                </li>
-                            </ul>    
-
-                            {/* <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent> */}
-                        </div>
+      {/* <CardComponent price={"5"} name={"T-Shirt 1"} category={"Men"} image={tshirt}></CardComponent> */}
+      
+      
+      {/* Elementor */}
+      <section class="elementor">
+        <div class="container-overlay">
+          <div class="elementor-background-overlay"></div>
+          <div class="elementor-content">
+            <div class="elementor-section">
+              <div class="elementor-container">
+                <div class="elementor-head">
+                  <h6 class="head">New Collection</h6>
                 </div>
-            </section>
-       
+                <div class="elementor-content-bold">
+                  <h2 class="content-bold">Be different in your own easy!</h2>
+                </div>
+                <div class="elementor-content-small">
+                  <h4 class="content-small">Find your unique style</h4>
+                </div>
+                <div class="shop-collection-button">
+                  <a href="" class="collection-button">
+                    Shop Collection
+                  </a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
