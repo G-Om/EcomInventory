5b62e26e99817f10e774b7bf8b07c73539b38030
om.gavade <115257951+G-Om@users.noreply.github.com>
2023-08-06 17:12:45 +0530

    Removed unnecessary text

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -5,7 +5,6 @@ export const MainComponent = ()=>{
 
     return(
         <div>
-            <h1> Main Component</h1>
             <HomePage></HomePage>
             <FooterComponent></FooterComponent>
         </div>
