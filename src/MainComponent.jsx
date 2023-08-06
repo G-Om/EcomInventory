2075339d9677d3e96b00b523932c0a7672f044de
om.gavade <115257951+G-Om@users.noreply.github.com>
2023-08-06 19:40:36 +0530

    Updated navbar component

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,10 +1,12 @@
 import { FooterComponent } from "./Components/FooterComponent"
+import { NavbarComponent } from "./Components/NavbarComponent"
 import { HomePage } from "./Pages/HomePage"
 
 export const MainComponent = ()=>{
 
     return(
         <div>
+            <NavbarComponent></NavbarComponent>
             <HomePage></HomePage>
             <FooterComponent></FooterComponent>
         </div>
