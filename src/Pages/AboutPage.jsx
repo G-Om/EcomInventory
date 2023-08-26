1c4ced686323e6392ed709b9b66b640fc8a5a2cd
Sreyash <sreyashkunkekar2889@gmail.com>
2023-08-26 20:00:13 +0530

    About page added

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,10 +1,103 @@
-
-
-export const AboutPage = ()=>{
-
-    return (
-        <div>
-            <h2>ABOUT</h2>
+export const AboutPage = () => {
+  return (
+    <div>
+      <section className="about-1">
+        <div className="elementor1">
+          <div className="elementor1-head">
+            <h1>About</h1>
+          </div>
+          <div className="elementor1-dash">
+            <span className="elementor1-dash-line"></span>
+          </div>
+          <div className="elementor1-dash">
+            <span></span>
+          </div>
+          <div className="elementor1-subHead">
+            <h5>
+              Proin eu ante vel mauris molestie dignissim non eget nunc. Integer
+              ac massa orci. Suspendisse vulputate semper nunc eget rhoncus.
+            </h5>
+          </div>
+          <div className="elementor1-subHeading">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu
+              ante vel mauris molestie dignissim non eget nunc. Integer ac massa
+              orci. Suspendisse vulputate semper nunc eget rhoncus. Ut sit amet
+              porta sem, interdum tincidunt libero. Nulla vel quam lobortis,
+              varius est scelerisque, dapibus nisl.
+            </p>
+          </div>
         </div>
-    )
+      </section>
+
+      <section className="about-2">
+         
+        <div className="elementor2-about">
+          <div className="elementor2-image-content-1">
+            <h6>The mission</h6>
+            <h2>
+              At the heart of everything, we set out to offer the best quality.
+            </h2>
+          </div>
+          <div className="elementor2-image-content-2">
+            <h4>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu
+              ante vel mauris molestie dignissim non eget nunc. Integer ac massa
+              orci. Suspendisse vulputate semper nunc eget rhoncus. Aenean
+              placerat facilisis ex, eu laoreet lorem convallis.
+              <br></br> <br></br>
+              Fusce gravida justo a lectus tempus lacinia. Ut mollis scelerisque
+              ultricies. Aenean facilisis efficitur magna, at feugiat massa
+              bibendum at. Etiam ut venenatis urna. 
+            </h4>
+          </div>
+         
+          </div>
+        
+      </section>
+
+      <section className="about-3">
+        <div className="elementor3-about">
+      
+          <div className="elementor3-head">
+            <h2>How it Started</h2>
+          </div>
+          <div className="elementor3-dash">
+            <span className="elementor3-dash-line"></span>
+          </div>
+          <div className="elementor3-sub-head">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+          </div>
+          
+
+          <div className="elementor3-container">
+          <div className="elementor3-sub-container">
+            <div className="container-text">
+              <h3>Vel mauris molestie dignissim</h3>
+              <h5>
+                Proin eu ante vel mauris molestie dignissim non eget nunc.
+                Integer ac massa orci. Suspendisse vulputate semper nunc eget
+                rhoncus.
+              </h5>
+              <p>
+                Praesent vel faucibus ligula. Sed sit amet ipsum eget velit
+                aliquet faucibus. Maecenas et odio id turpis sollicitudin
+                pulvinar sit amet vitae augue. Phasellus nec ultricies arcu.
+                Quisque efficitur tellus sit amet bibendum molestie. Duis id
+                egestas odio. Phasellus lacinia ex quis faucibus tempor. Sed
+                feugia.
+              </p>
+            </div>
+            <div className="container-image">
+                
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
